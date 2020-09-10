095d267535764dbc801d4756c17bf54a6784d341
JuliaNastenko <juli.nastenko@gmail.com>
2020-09-10 16:45:03 +0300

    Updates

---
+++ precache-manifest.f9520599d9ed5c5a9e91e2e4b2af6c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c90baf1947da2a89d998d930599eae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc59d9cac6b159eaa643",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "1a5ab6f61ec13e4308e4",
+    "url": "./static/js/2.6da940cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6da940cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc59d9cac6b159eaa643",
+    "url": "./static/js/main.06e65c1b.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
